f71c0b0f1289673aec511261831ca2b072bdf2c0
Kshitij Chauhan <ckshitij2@gmail.com>
2024-05-28 17:34:51 +0530

    using Pug

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path=require('path')
 const rootDir=require('./util/path')                                                               
 const app=express();
 
-
+app.set('view engine','pug')
+app.set('views','views')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(rootDir,"public")))
 
@@ -16,13 +17,12 @@ app.use((req,res,next)=>{
     }
     next()
 })
-app.use(adminRoutes)
-
+app.use('/admin', adminRoutes.routes);
 app.use(shopRoutes)
 
 app.use((req,res,next)=>{
     const htmlFilePath=path.join(__dirname,'/views','error404.html')
-    res.status(404).sendFile(htmlFilePath)
+    res.status(404).render('error404',{pageTitle:'404'})
 })
 
 
--- routes/admin.js
+++ routes/admin.js
@@ -2,19 +2,22 @@ const express=require('express')
 const path=require('path')
 const rootDir=require('../util/path')
 const router=express.Router()
+const products=[]
 
 router.get('/add-product',(req,res,next)=>{
     const htmlFilePath=path.join(rootDir,'/views','add-product.html')
-    res.sendFile(htmlFilePath)
+    res.render('add-product',{pageTitle:'Add Product',path:'/admin/add-product'})
 
 })
 
 router.post('/add-product',(req,res,next)=>{
     console.log(req.body)
+    products.push({title:req.body.title})
     res.redirect('/')
 
 
 })
 
 
-module.exports=router
+exports.routes=router
+exports.products=products
